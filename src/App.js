b747693825210f92596bbe0a94b3c9a5a6f77aab
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-09-16 12:29:21 +0200

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './form/index.js';
 import Tasks from './tasks/index.js';
@@ -6,19 +7,29 @@ import SectionButtons from './section-buttons/index.js';
 import Header from './header/index.js';
 import Container from './container/index.js';
 
-let tasks = [
-  { id: 1, content: "pozmywać naczynia", done: false },
-  { id: 2, content: "odrobić zadanie z YouCode", done: true },
-];
-
-let readyTasksHidden = false;
-
 function App() {
+  const [readyTasksHidden, setReadyTasksHidden] = useState(false);
+  const [tasks, setTasks] = useState([
+    { id: 1, content: "pozmywać naczynia", done: false },
+    { id: 2, content: "odrobić zadanie z YouCode", done: true },
+  ]);
+
+  const addNewTask = (content) => {
+    setTasks (tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.lenght ? tasks[tasks.lenght - 1].id + 1 : 1,
+      }
+    ])
+  }
+
   return (
     <Container>
       <Header title="Lista zadań" />
       <Section title="Dodaj nowe zadanie"
-        body={<Form />}
+        body={<Form addNewTask={addNewTask} />}
       />
       <Section title="Lista zadań"
         body={<Tasks tasks={tasks} readyTasksHidden={readyTasksHidden} />}

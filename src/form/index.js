7122deabc6a08b91cb5cdef74f0cefa06dae84ef
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-09-19 11:49:50 +0200

    added styled.js to form

--- src/form/index.js
+++ src/form/index.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./style.css";
+import { Form as StyledForm, Input, Button } from "./styled"
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
@@ -11,16 +11,15 @@ const Form = ({ addNewTask }) => {
     };
 
     return (
-        <form className="form" onSubmit={onFormSubmit}>
-            <input
+        <StyledForm onSubmit={onFormSubmit}>
+            <Input
                 type="text"
-                className="form__input"
                 placeholder="Co jest do zrobienia?"
                 value={newTaskContent}
                 onChange={({ target }) => setNewTaskContent(target.value)}
             />
-            <button className="form__button">Dodaj zadanie</button>
-        </form>
+            <Button>Dodaj zadanie</Button>
+        </StyledForm>
     );
 };
 
---
+++ src/form/styled.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+    display: flex;
+    padding: 20px;
+
+    @media (max-width: 767px){
+        flex-wrap: wrap;
+    }
+`
+
+export const Input = styled.input`
+    flex-basis: 100px;
+    flex-grow: 1;
+    overflow: hidden;
+    padding: 7px;
+    border: 1px solid #ddd;
+`
+
+export const Button = styled.button`
+    padding: 7px;
+    border: none;
+    background-color: teal;
+    color: #fff;
+    margin-left: 15px;
+    transition: 0.2s;
+
+    @media (max-width: 767px) {
+        flex-grow: 1;
+        flex-basis: 100%;
+        margin: 10px 0; 
+    }
+
+    &:hover {
+        background-color: hsl(180, 100%, 27%);
+        transform: scale(1.1);
+    }
+
+    &:active {
+        background-color: hsl(180, 100%, 30%); 
+    }
+`

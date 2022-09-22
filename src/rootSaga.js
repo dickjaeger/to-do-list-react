35d813ed19ad4d5f41ec53bb6cf9feb38d56d8c5
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-09-22 20:18:25 +0200

    trash removed

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,8 @@
 import { all } from 'redux-saga/effects'
 import { tasksSaga } from './features/tasks/tasksSaga';
-// import { saga2 } from "./saga2";
 
 export default function* rootSaga() {
     yield all([
         tasksSaga(),
-        //saga2()
     ]);
 };

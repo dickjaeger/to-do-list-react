0254221e26f554dfa74e95c4d00ad57e60b2cae8
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-09-23 16:08:04 +0200

    added image and styled.js to author page

---
+++ src/features/author/AuthorPage/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+    display: block;
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+    border-radius: 50%;
+    margin: 0 auto 20px;
+    box-shadow: #ddd 2px 2px 5px;
+`
+
+export const Header = styled.h1`
+    text-align: center;
+    margin-top: 0;
+`

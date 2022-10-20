ec5175a20e629159c69c10df66c6224b3b79736c
Wojciech Leksza <wojtek.leksza@gmail.com>
2022-10-20 10:13:23 +0200

    Author page content updated

--- src/features/author/AuthorPage/index.js
+++ src/features/author/AuthorPage/index.js
@@ -26,52 +26,51 @@ const AuthorPage = () => (
         <h3>Spawanie</h3>
         <p>
           Zawodowo pracowałem głównie jako <span>ślusarz/spawacz</span>.
-          Wraz z
-          zespołem wykonywaliśmy konstrukcje stalowe dla dużych
-          firm z
-          <em>całego świata</em>,
-          takich jak np. <i>Bayer</i> czy <i>Coca-Cola</i>.
+          Wraz z zespołem wykonywaliśmy konstrukcje stalowe dla dużych
+          firm z <em>całego świata</em>, takich jak np. <i>Bayer </i>
+          czy <i>Coca-Cola</i>.
         </p>
         <h3>Programowanie</h3>
         <p>
           Aktualnie skupiam się na nauce programowania. Od zawsze
           chiałem to robić. Wcześniej
           hobbystycznie rozwijałem swoje umiejętności w
-          programowaniu
-          sterowników PLC i mikrokontrolerów.
+          programowaniu sterowników PLC i mikrokontrolerów.
           Ostatecznie jednak wybrałem <i>Frontend</i>, dlatego
-          dołączyłem do kursu
-          <strong> YouCode</strong>.
+          dołączyłem do kursu <strong>YouCode</strong>.
         </p>
       </>}
     />
     <Section
       title="Moje Hobby"
       body={<>
+        <h3>Elektronika i programowanie</h3>
+        <p>
+          Już od dzieciństwa najbardziej interesowały mnie 
+          zabawki elektroniczne. Nie używałem ich jednak w 
+          konwencjonalny sposób - większość takich zabawek 
+          rozkładałem na czynniki i badałem ich działanie. 
+          Później, w okresie szkolnym, uczyłem się samemu 
+          tworzyć różnorakie konstrukcje. Gdy chodziłem do 
+          gimnazjum, zakupiłem moją pierwszą ATMegę - tak 
+          zaczęła się moja przygoda z programowaniem. 
+          Aktualnie mało zajmuję się elektroniką (jedynie 
+          drobne naprawy domowych sprzętów), bardziej 
+          skupiam się na programowaniu, a dokładniej na
+          tworzeniu aplikacji internetowych. 
+        </p>
         <h3>Akwarystyka</h3>
         <p>
-          Tym tematem zainteresowałem się, gdy mój tata kupił
-          nasze
-          pierwsze akwarium.
-          Był to mały zbiornik, miał jedynie 60 litrów. Nie
-          przeszkadzało to jednak w kształtowaniu
-          mojej pasji. Teraz, kiedy jestem już na swoim w końcu
-          mogłem
-          sobie pozwolić na coś ciut
-          większego - 200 litrów. Obecnie to akwarium jest w fazie
-          dojrzewania i mam nadzieję, że
-          równolegle z jego dojrzewaniem, będą się rozwijały moje
-          umiejętności programowania. :)
-        </p>
-        <h3>Motocykle</h3>
-        <p>
-          Pomimo tego, że jeszcze nie mam prawa jazdy na
-          motocykle,
-          zawsze zajmowały one specjalne
-          miejsce w moim sercu. Od małego lubiłem grzebać przy
-          swoich
-          motorowerach i niezmiennie jest
-          to mój ulubiony środek transportu.
+          Tym tematem zainteresowałem się, gdy mój tata kupił 
+          nasze pierwsze akwarium. 
+          Był to mały zbiornik, miał jedynie 60 litrów. Nie 
+          przeszkadzało to jednak w kształtowaniu mojej pasji. 
+          Teraz, kiedy mam już swoje mieszkanie, w końcu 
+          mogłem pozwolić sobie na coś trochę większego - 
+          200 litrów. Obecnie moje akwarium jest w fazie 
+          dojrzewania i mam nadzieję, że równolegle z jego 
+          dojrzewaniem, będą się rozwijały moje umiejętności 
+          programowania. :)
         </p>
       </>}
     />
